7d37a2563a318739288c2308f9b242385936bc55
kristen-howton <kristen.howton@gmail.com>
2020-02-24 10:48:21 -0600

    Added Finley the betta fish

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -49,6 +49,15 @@ const initializeDetailButtonEvents = () => {
             theDialog.showModal()
         }
     )
+
+    // Show Bart's details when the button is clicked
+    document.querySelector("#button--finley").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--finley")
+            theDialog.showModal()
+        }
+    )
 }
 
 export default initializeDetailButtonEvents
